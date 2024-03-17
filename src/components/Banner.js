5bf55c83766c8cf6538e1aab5e13b2c4a7066635
Damir Maksut <damirmaksut@Damirs-MacBook-Pro.local>
2024-03-17 18:26:41 +0100

    first commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Design", "Business Consulting" ];
+  const toRotate = [ "Web Developer", "Design of your robot", "Business Consulting in Jeme" ];
   const period = 2000;
 
   useEffect(() => {
